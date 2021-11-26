5834783c68c9c5cf8da0d503137f568d625504eb
Trandinh Thien <trandit@mcmaster.ca>
2021-11-26 16:56:10 -0500

    Added initial assets, and created a working UI for navigating from and to main menu

---
+++ UnityProjectFile/Assets/Code/GameDirector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour
+{
+    private GameObject MenuCanvas; //gameobject that contains canvas of main menu
+    private GameObject InGameManager; //gameobject that contains aspects related to in-game
+    private GameObject InGameUI;
+
+    void Awake()
+    {
+        MenuCanvas = GameObject.FindGameObjectWithTag("MenuCanvas");
+        InGameManager = GameObject.FindGameObjectWithTag("InGameManager");
+        InGameUI = InGameManager.transform.Find("UICanvas").gameObject;
+
+        if (MenuCanvas == null) Debug.Log("MenuCanvas not found");
+        if (InGameManager == null) Debug.Log("InGameManager not found");
+        if (InGameUI == null) Debug.Log("InGameUI not found");
+
+        InGameManager.SetActive(false);
+    }
+
+    //this function will be called by MenuUI's sendMessage
+    public void StartGame()
+    {
+        MenuCanvas.SetActive(false);
+        InGameManager.SetActive(true);
+    }
+
+    //this function will be called by MenuUI's sendMessage
+    public void HowToPlay()
+    {
+        MenuCanvas.SetActive(false);
+        //TODO
+    }
+
+    //this function will be called if player returns to menu
+    public void BackToMenu()
+    {
+        MenuCanvas.SetActive(true);
+        InGameManager.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ UnityProjectFile/Assets/Code/UI/MenuUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour
+{
+
+    //sends message upwards to start game
+    public void clickStartGame()
+    {
+        SendMessageUpwards("StartGame");
+    }
+
+    //exits application
+    public void clickExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void clickHowToPlay()
+    {
+        SendMessageUpwards("HowToPlay");
+    }
+
+    public void clickReturnToMenu()
+    {
+        SendMessageUpwards("BackToMenu");
+    }
+
+}
